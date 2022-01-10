be066ff1ffc6cce2480e360908f12d58a15c5d75
BluDood <70075053+BluDood@users.noreply.github.com>
2022-01-10 22:00:48 +0100

    Another update! The date on the top of the screen is now dynamic Repo Adding prompt when getting a tweak (can be disabled) Minor Improvements&Fixes

--- assets/index.js
+++ assets/index.js
@@ -37,7 +37,7 @@ async function addFeatured(name, icon, banner, repo, id) {
     const card = document.createElement('div')
     card.classList.add("card")
     card.style.backgroundImage = `url(${banner})`
-    card.innerHTML = ` <div class="filler"></div> <div class="install"> <img src="${icon}" alt="Icon for package ${name}"> <h3 class="title">${name}</h3> <div class="filler"></div> <a href="cydia://package/${id}" class="get"> <span>GET</span> </a> </div>`
+    card.innerHTML = ` <div class="filler"></div> <div class="install"> <img src="${icon}" alt="Icon for package ${name}"> <h3 class="title">${name}</h3> <div class="filler"></div> <a href="" onclick="repoPrompt('${name}', '${repo}', '${id}'); return false" class="get"> <span>GET</span> </a> </div>`
     document.querySelector(".featured").append(card)
 }
 
@@ -49,46 +49,58 @@ async function addRepos(name, icon, repo) {
     document.querySelector(".popular").append(card)
 }
 
-/*
-function showAlert() {
-    var notification = document.querySelector(".notification")
-    if (notification.classList.contains("shown")) {
-        notification.classList.remove("shown")
-    } else {
-        notification.classList.add("shown")
-    }
-    return false
-}
-document.querySelector('.notification').addEventListener('click', function (event) {
-    const array = []
-    for (i in event.path) {
-        array.push(event.path[i].className)
-    }
-    if (!array.join(" ").includes("inner")) {
-        showAlert()
-    }
-})
-*/
-
-function repoPrompt(name, reponame, repo) {
-    iOSalert("add", `Add ${reponame} to install ${name}!`, ``)
-}
-
-function iOSalert(option, title, content) {
+// Function for displaying iOS-styled alerts
+function iOSalert(option, title, content, buttons) {
     if (option === 'add') {
+        const btnArray = []
+        buttons.forEach(item => {
+            btnArray.push(`<a href="${item.link}">${item.name}</a>`)
+        });
         const notification = document.createElement('div')
         notification.classList.add('notification')
         notification.id = "notification"
-        notification.innerHTML = `<div class="inner"> <h2 class="title">${title}</h2> <span class="content">${content}</span> <div class="filler"></div> <div class="actions"> <a href="" onclick="iOSalert('remove', 'Hello', 'testing'); return false" class="default">OK</a> </div> </div>`
+        notification.innerHTML = `<div class="inner"> <h2 class="title">${title}</h2> <span class="content">${content}</span> <div class="filler"></div> <div class="actions"> ${btnArray.join(" ")} <a href="" onclick="iOSalert('remove'); return false" class="default">Close</a> </div> </div>`
         document.body.append(notification)
         setTimeout(() => {
             document.getElementById('notification').classList.add('shown')
         }, 50);
     } else {
-        document.getElementById('notification').remove()
+        document.getElementById('notification').classList.remove('shown')
+        setTimeout(() => {
+            document.getElementById('notification').remove()
+        }, 250);
     }
 }
 
+// Prompt for adding a repository when adding a tweak, using the iOSalert function
+async function repoPrompt(name, url, id) {
+    if (localStorage.getItem('directGet') === 'true') return window.location = `cydia://package/${id}`
+    iOSalert("add", 'Notice', `Add ${url} to install ${name}!`, [{
+        "name": "Add Repository and Open",
+        "link": `javascript:getTweak('${url}', '${id}')`
+    },
+    {
+        "name": "Do not show this again",
+        "link": "javascript:localStorage.setItem('directGet', 'true'); iOSalert('remove')"
+    }])
+}
+
+// Add a repo and get a tweak
+async function getTweak(url, id) {
+    window.location = `cydia://url/https://cydia.saurik.com/api/share#?source=${url}`
+    setTimeout(() => {
+        window.location = `cydia://package/${id}`
+        iOSalert('remove')
+    }, 1000);
+}
+
+// Set the date for the header
 var now = new Date()
 
-var date = today.getMonth + today.getDay
+var date = now.toLocaleDateString('en-us', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric'
+})
+
+document.querySelector('.h-date').innerText = date
